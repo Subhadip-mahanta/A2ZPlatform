96a1e9327ade31b89b1625b31f63c394e1cfa902
Subhadip-mahanta <122591537+Subhadip-mahanta@users.noreply.github.com>
2024-04-06 12:52:34 +0530

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap css link -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- fontawesome css link -->
+    <link rel="stylesheet" href="css/all.min.css">
+    <!-- custom css link -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/laptop.css" media="screen and (max-width:1300px)">
+    <link rel="stylesheet" href="css/mobile.css" media="screen and (max-width:570px)">
+
+    
+    <title>Edusoftwaresolu</title>
+</head>
+<body>
+
+  
+     <!-- start navigation -->
+     <nav class="navbar navbar-expand-sm bg-body-tertiary bg-dark border-bottom border-body p-2 fixed-top" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand brand-text" href="index.php">A2Zplatform</a>
+    <!-- <span class="navbar-text">Learn new Technology</span> -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse MyNav" id="navbarNav">
+      <ul class="navbar-nav custom-nav pl-2">
+
+        <li class="nav-item custom-nav-item" >
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="courses.php">Courses</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Payment Status</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">My Profile</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Logout</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#StudentLoginModel">Login</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="" data-bs-toggle="modal" data-bs-target="#StudentRegisterModel">Signup</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link " href="#Contact">Contact</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
+     <!-- end navigation -->
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam mollitia voluptatibus facere molestias iste? Fugiat dolor aliquid velit, ut mollitia harum beatae alias sunt qui nostrum repudiandae labore quas consequuntur voluptatum aliquam voluptates ullam? In quasi eveniet quisquam minima ullam quod dolores, exercitationem ipsam accusamus voluptatem iste vero, consequatur voluptas ipsum deserunt numquam atque fuga repellendus pariatur unde suscipit dolore quos officiis vel! Nisi unde atque tempora assumenda tempore eos, ratione illum. Recusandae sapiente architecto eos dolorum! Libero voluptas eos eligendi modi ratione excepturi ut, tempore voluptates itaque debitis! Doloribus debitis, non enim distinctio obcaecati repellat rerum a neque, similique impedit possimus asperiores nemo consequatur quas ducimus? In laborum voluptates, beatae, sed aliquam modi eveniet blanditiis corrupti tempora maxime error esse velit, sapiente quaerat saepe molestiae omnis quos? Fugiat doloremque iusto unde, vitae, qui vel in autem sequi distinctio, optio adipisci laborum veniam. Totam laborum ratione perspiciatis dignissimos molestias quaerat quis doloremque eum sint culpa ex rem repudiandae eaque iure facilis, sequi possimus praesentium quibusdam assumenda impedit iste modi repellendus quidem! Adipisci minima ipsa nesciunt officia laborum veritatis, aliquid atque consectetur, illo amet quia in et nisi hic numquam cumque eaque possimus. Debitis exercitationem culpa labore nostrum? Aspernatur distinctio repellendus minus animi nobis amet error beatae quasi tempora cumque veritatis, sapiente rerum et quibusdam voluptas doloribus commodi possimus atque! Error veritatis veniam nisi quidem tempora nulla tempore quibusdam hic harum laudantium odit atque facilis voluptatum dolorum officiis repellat, mollitia aliquam ab dignissimos unde at quo quia. Dolorem et aspernatur delectus deleniti dolore accusamus assumenda. Accusamus repudiandae illum, impedit debitis minus aliquam et. Ipsa veniam quod incidunt pariatur, quae voluptatibus quis velit eveniet, ipsam repudiandae iusto tempore natus. A ea quas provident molestias soluta suscipit nihil aut, eum nam inventore error autem itaque explicabo alias deserunt voluptas odit consequatur quae praesentium omnis porro fugiat! Ea nihil aliquam rerum doloremque veritatis, at omnis sed ipsa incidunt voluptate necessitatibus illum voluptatum architecto? Dolorum odio quae eligendi ducimus libero quos excepturi alias explicabo, qui nesciunt est, tenetur ut provident earum. Eaque vel molestias sit recusandae ullam sunt dolorem eum consequatur totam quisquam. Officia, temporibus assumenda? Expedita reprehenderit explicabo molestiae, a fuga quaerat sunt voluptate ab sapiente nostrum deserunt voluptas culpa reiciendis iste cupiditate. Libero earum facere, ipsam, temporibus omnis nostrum quam autem rerum et, esse maxime quasi pariatur in nam nemo eaque voluptatum officia quod. Nihil iusto maxime voluptate nemo illo, incidunt deserunt corporis quidem reprehenderit adipisci nulla perspiciatis. Laboriosam ad tempore facere aspernatur sint. Autem aspernatur perferendis, nihil est dolore ullam minima similique adipisci magni ipsam possimus aperiam fugit culpa neque cumque voluptatum unde veritatis ducimus modi porro. Sed, voluptatem rerum voluptate excepturi minima distinctio omnis aut laudantium, doloremque ut nam beatae eos totam qui natus minus id porro? Veritatis provident, enim asperiores, eveniet sapiente quo natus obcaecati odio illo, aperiam sunt accusamus quae. Excepturi sapiente neque reiciendis ullam, magni quam soluta provident incidunt modi id in sint, eos minima omnis sit! Fugiat quam cumque minus blanditiis natus sint, ex exercitationem, consectetur ratione ipsa id illum inventore ad. Libero illo laudantium consequuntur error recusandae illum itaque aut odit at architecto repellat deserunt suscipit consequatur laboriosam maxime voluptate soluta eum facilis maiores dolor, vel doloremque magnam quibusdam! Adipisci sint molestiae sed veniam culpa at cumque, eos eius neque. Repellat quaerat architecto iure officia? Aperiam facere quasi ad debitis tempore in fugiat culpa voluptas, earum maiores ipsum nisi amet vitae. Neque doloremque unde illo quod modi ipsam laboriosam impedit temporibus, corrupti ullam molestiae necessitatibus, minus repellat ipsa sit commodi repudiandae, et voluptates saepe sapiente quisquam similique. Modi ullam architecto odio harum dolor autem tempora distinctio similique impedit vitae deserunt sapiente commodi optio id iure exercitationem repellat minus, est aperiam temporibus quas voluptas laudantium voluptatem repellendus. Provident consequuntur nemo aspernatur nam ad officiis repudiandae sit deleniti dignissimos velit magni harum possimus ullam facilis, assumenda reprehenderit. Numquam doloribus nesciunt inventore, quisquam asperiores impedit nobis! Ex, veniam consequuntur rerum asperiores accusantium commodi quasi laboriosam ad amet odio, quaerat, cum reprehenderit quia eaque ducimus tenetur facere doloremque quod similique aliquid. Obcaecati praesentium natus dolorem nesciunt voluptatibus non cum excepturi, laborum minima, neque repellat quia ex odit eos harum dignissimos vero aliquid exercitationem error voluptatum. Tempore, blanditiis excepturi animi iure exercitationem cupiditate, quis quos nostrum culpa maiores cum tenetur vel perferendis fuga harum eos nobis libero. Dolores quaerat accusantium quisquam ipsum magni doloribus. Laudantium earum dolores explicabo consectetur repellendus, illum quis exercitationem culpa quibusdam quas libero nostrum! Dignissimos esse aspernatur in alias distinctio maiores eveniet doloribus facilis! Laudantium et vel dicta sed eos optio. Nemo nulla, ipsa obcaecati perspiciatis, eaque quae asperiores quia possimus maxime optio repellat sint consequuntur, suscipit quibusdam totam iste aliquid impedit molestiae culpa illo repellendus? Nostrum, necessitatibus. Corporis suscipit dolores aperiam, natus non voluptatem optio odio, molestias velit animi possimus reprehenderit quo, porro molestiae esse. Quis rerum dolore deleniti architecto blanditiis provident, iste eligendi quibusdam quod nam soluta est libero? Voluptates, totam quidem quisquam dolore id non debitis expedita tenetur aperiam accusamus nobis optio nisi iste quia excepturi inventore eum odit, repudiandae eos voluptate. Reiciendis magnam libero nostrum officia animi facere illum debitis? Sed, iste. Magni, aliquam harum ipsum enim quibusdam officia cumque laborum, similique sed sit laudantium neque assumenda quaerat! Minus facere esse voluptatibus iste pariatur odio, natus ut debitis error adipisci provident ratione eligendi nam dolorum optio ipsa enim accusamus. Reiciendis explicabo praesentium ratione. Corporis, dicta odit. Laborum ipsum, debitis nihil asperiores incidunt, amet dolor eaque quas vel architecto iusto rem voluptatum, iste voluptatem itaque ratione. Voluptas iste necessitatibus possimus aliquid fugit accusamus tempora molestias quae eaque dolor! Facere atque, rem vero similique sint ipsum iusto nulla facilis laborum repellat culpa magnam officiis fuga repudiandae! Reprehenderit et dolorum vel dolore est, fugit explicabo aliquam velit sequi aut consequuntur iusto necessitatibus? Consequuntur, doloremque. Ea cum nemo eius saepe quos, corrupti illo dicta eligendi laborum quisquam perspiciatis, ab iste consequuntur? Ut, aliquam vel. Facilis aliquam deserunt reiciendis repellat nihil ratione obcaecati omnis iusto sint pariatur, fuga consequatur consequuntur incidunt officia blanditiis itaque. Iure consequatur molestias deserunt.</p>
+
+
+
+
+            
+    <!-- student login modelform popup  start here -->
+<!-- Modal -->
+<div class="modal fade" id="StudentLoginModel" tabindex="-1" aria-labelledby="StudentLoginModelLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="StudentLoginModelLabel">Student Login</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+           <!-- student Login form start here -->
+          <form id="LoginForm">
+     
+  <div class="mb-3">
+  <i class="fas fa-envelope"></i>
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="LoginEmail" aria-describedby="emailHelp" name="Lemail" placeholder="E-mail">
+  </div>
+  <div class="mb-3">
+  <i class="fas fa-key"></i>
+    <label for="exampleInputPassword1" class="form-label">New Password</label>
+    <input type="password" class="form-control" id="LoginPassword" name="Lpassword" placeholder="Password">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Login</button>
+</form>
+     <!-- student register form end here -->     
+   </div>     
+  </div>
+  </div>
+</div>
+    <!--end here StudentLogin Model form popup -->
+
+       
+    <!-- start Registration form popup -->
+<!-- Modal -->
+<div class="modal fade" id="StudentRegisterModel" tabindex="-1" aria-labelledby="StudentRegisterModelLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="StudentRegisterModelLabel">Student Registration</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+           <!-- student register form start here -->
+           <form id="RegisterForm">
+     <div class="mb-3">
+      <i class="fas fa-user"></i>
+    <label for="exampleInputName" class="form-label">Name</label>
+    <input type="text" class="form-control" id="RegisterName" aria-describedby="emailHelp" name="Rname" placeholder="Name">
+  </div>
+  <div class="mb-3">
+  <i class="fas fa-envelope"></i>
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="RegisterEmail" aria-describedby="emailHelp" name="Remail" placeholder="E-mail">
+  </div>
+  <div class="mb-3">
+  <i class="fas fa-key"></i>
+    <label for="exampleInputPassword1" class="form-label">New Password</label>
+    <input type="password" class="form-control" id="RegisterPassword" name="Rpassword" placeholder="Password">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Signup</button>
+</form>
+     <!-- student register form end here -->  
+   </div>     
+  </div>
+  </div>
+</div>
+    <!--end here Registration form popup -->
+
+     <!-- start contact us -->
+     <div class="container mt-4" id="Contact">
+          <h2 class="text-center mb-4">Contact Us</h2>
+          <div class="row">
+             <div class="col-md-8">
+                <form action="" method="post">
+                   <input type="text" class="form-control" name="name" placeholder="Name"><br>
+                   <input type="text" class="form-control" name="subject" placeholder="Subject"><br>
+                   <input type="email" class="form-control" name="email" placeholder="E-mail"><br>
+                   <textarea name="message" class="textarea1" placeholder="How can we help you?" style="height:150px; resize:none"></textarea><br><br>
+                   <input type="submit" class="btn btn-outline-primary" value="Send" name="submit"><br><br>
+                </form>
+             </div>
+             <!-- end contact us 1 column -->
+             <!-- start contact us 2 column -->
+              <div class="col-md-4 stripe text-white text-center">
+                <h4>A2Zplatform</h4>
+                <p> A2Zplatform, 
+                    Near Bishnupur MIT College, Bankura,
+                    West Bengal -722158 <br>
+                    Phone: +919564747935 <br>
+                    www.a2zplatform.com <br>
+                </p>
+              </div>
+             <!-- end contact us 2 column -->
+
+          </div>
+
+       </div>
+    <!-- ends contact us -->
+
+
+
+      <!-- start footer here -->
+      <div class="footer bg-dark text-white p-4" >
+             
+            <h6>Copyright &copy; 2024 .All Right Reserved </h6>
+      </div>
+    <!-- ends footer here -->
+
+
+
+
+
+<!-- jquery link -->
+<script src="jquery/jquery-3.6.3.min.js"></script>
+<!-- bootstrap js link -->
+<script src="js/bootstrap.min.js"></script>
+<!-- fontawesome link -->
+<script src="js/all.min.js"></script>
+</body>
+</html>
